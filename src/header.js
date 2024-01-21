e63ecf4d03811392be314a26c539b73187de879e
Darknab <117382045+Darknab@users.noreply.github.com>
2024-01-21 19:04:26 +0100

    Export a node from header.js instead of a function that append directly the node to the DOM.

--- src/header.js
+++ src/header.js
@@ -1,21 +1,20 @@
-export function displayHeader() {
-  const header = document.createElement('div');
-  header.id = 'header';
+const header = document.createElement('div');
+header.id = 'header';
 
-  const list = document.createElement('ul');
+const list = document.createElement('ul');
 
-  const home = document.createElement('li');
-  home.innerHTML = "<a href=\"#\">Home</a>"
+const home = document.createElement('li');
+home.innerHTML = "<a href=\"#\">Home</a>"
 
-  const menu = document.createElement('li');
-  menu.innerHTML = "<a href=\"#\">Menu</a>";
+const menu = document.createElement('li');
+menu.innerHTML = "<a href=\"#\">Menu</a>";
 
-  const services = document.createElement('li');
-  services.innerHTML = "<a href=\"#\">Services</a>";
+const services = document.createElement('li');
+services.innerHTML = "<a href=\"#\">Services</a>";
 
-  list.append(home, menu, services);
+list.append(home, menu, services);
 
-  header.appendChild(list);
+header.appendChild(list);
 
-  document.body.appendChild(header);
-}
+export { header };
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import '../src/assets/stylesheets/style.css';
-import { displayHeader } from '../src/header';
+import { header } from '../src/header';
 import { hero } from './hero';
 
 const content = document.createElement('div');
 content.id = 'content';
 
-displayHeader();
+document.body.appendChild(header);
 document.body.appendChild(content);
 content.appendChild(hero);
