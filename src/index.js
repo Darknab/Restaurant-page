373970926968281a374d0b7cf32f87df0550e6b9
Darknab <117382045+Darknab@users.noreply.github.com>
2024-01-23 10:24:11 +0100

    Add logic to switch display of content when the user click on the corresponding button.

--- src/index.js
+++ src/index.js
@@ -10,7 +10,59 @@ const content = document.querySelector('#content');
 document.body.prepend(header);
 
 content.appendChild(hero);
-content.appendChild(menu);
-content.appendChild(services);
 
 document.body.append(footer, credentials);
+
+addEventListener('click', (e) => {
+  console.log(e.target.id);
+  switch (e.target.id) {
+    case 'home-btn': {
+      const homeBtn = document.querySelector('#home-btn');
+      console.log(`${homeBtn.id} says: ouch!`);
+      activateTab(homeBtn);
+      clearContent();
+      content.appendChild(hero);
+      break;
+    }
+    case 'menu-btn': {
+      const menuBtn = document.querySelector('#menu-btn');
+      console.log(`${menuBtn.id} says: ouch!`);
+      activateTab(menuBtn);
+      clearContent();
+      content.appendChild(menu);
+      break;
+    }
+    case 'services-btn': {
+      const servicesBtn = document.querySelector('#services-btn');
+      console.log(`${servicesBtn.id} says: ouch!`);
+      activateTab(servicesBtn);
+      clearContent();
+      content.appendChild(services);
+      break;
+    }
+  }
+});
+
+function checkActiveTab() {
+  const active = document.querySelector('.active');
+  return active;
+}
+
+function activateTab(tab) {
+  const active = checkActiveTab()
+  console.log(`${active.id} is active`)
+  if (active.id === tab.id) {
+    return;
+  } else {
+    active.classList.toggle('active');
+    console.log(`${active.id} is desactivated`);
+    tab.classList.toggle('active');
+    console.log(`${tab.id} is activated!`);
+  }
+}
+
+function clearContent() {
+  const content = document.querySelector('#content');
+  content.innerHTML = "";
+}
+
